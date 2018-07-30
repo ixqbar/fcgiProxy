1a07776105a13379e18f9ffbe6639398bd73068c
xingqiba <ixqbar@gmail.com>
2018-07-30 12:36:00 +0800

    optimize

--- example/send_notification_by_redis.php
+++ example/send_notification_by_redis.php
@@ -2,4 +2,4 @@
 
 $redis_handle = new Redis();
 $redis_handle->connect('127.0.0.1', 6899);
-$redis_handle->rawCommand("npush", "*", '{"noticeData":{"title":"pay","message":"nice","category":"notice"}}');
+$redis_handle->rawCommand("npush", "*", '{"title":"pay","message":"nice"}');
